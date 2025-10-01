86825700dab81442d88ceabc2be1777f78059407
Divyanshu Kumar Jha <divyanshukumarjha47@gmail.com>
2025-10-01 10:30:59 +0530

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,26 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function play(playerChoice) {
+  const choices = ["rock", "paper", "scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = `Tie! Both chose ${playerChoice}`;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = `You Win! ${playerChoice} beats ${computerChoice}`;
+    playerScore++;
+  } else {
+    result = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    computerScore++;
+  }
+
+  document.getElementById("result").innerText = result;
+  document.getElementById("score").innerText = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
